e203c7ade727a3d54a0bdbfad4f103f4b1e99043
KUNAL <imkkunal@gmail.com>
2026-09-19 12:02:24 +0530

    Fix production CORS

--- backend/main.py
+++ backend/main.py
@@ -130,14 +130,27 @@ app = FastAPI(
 # ==========================================================
 
    
-   app.add_middleware(
+ 
+ app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://bangalore-traffic-prediction.vercel.app"
+    ],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-   
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
  
  
 
